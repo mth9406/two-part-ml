bd5660aefc122cddc74572a5d65813ae53d8d022
mth9406 <51608554+mth9406@users.noreply.github.com>
2021-12-30 10:01:42 +0900

    added comma

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name= 'two_part_ml'
+    name= 'two_part_ml',
     version= '0.0.1',
     description= 'Implemenation of two-part-ml',
     author= 'SUNGWOO HUR',
